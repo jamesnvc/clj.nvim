c076ed7defe2e336069140a61634fa8dc59ad1ff
James N. V. Cash <james.nvc@gmail.com>
2017-04-27 18:56:02 -0400

    initial import

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-nvim
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_nvim/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-nvim.core-test
+  (:require [clojure.test :refer :all]
+            [clj-nvim.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
