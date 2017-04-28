671d33f28446b36f1ea42639bf21a81bc148cdb9
James N. V. Cash <james.nvc@gmail.com>
2017-04-28 19:20:22 -0400

    Basic require rewriting working

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [jamesnvc/neovim-client "0.1.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
+                 [jamesnvc/neovim-client "0.1.1"]
+                 [rewrite-clj "0.6.0"]])
